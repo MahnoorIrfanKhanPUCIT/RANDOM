4faeeb605f22cadb7c378d6c3718ff8e5e7ef98f
MahnoorIrfanKhanPUCIT <mahnoorkhanPUCIT@gmail.com>
2023-06-23 00:36:51 +0500

    First commit

---
+++ DSA-P1/Source.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+//FILE HANDLING
+int main()
+{
+	//FOR WRITING
+
+	//ofstream outf("test.txt");
+
+	/*if (!outf)
+	{
+		cerr << "FILE NOT FOUND..." << endl;
+		exit(0);
+	}*/
+
+	//outf << "HELLO WORLD" << endl;
+
+	//TAKING INPUT AND WRITING ON FILE
+
+	/*int id = 0;
+	float cgpa = 0.0f;*/
+
+	//cout << "Enter ID and CGPA : " << endl;
+
+	/*while (cin >> id >> cgpa)
+	{
+		outf << id << "   " << cgpa << endl;
+		cout << "Enter ID and CGPA : " << endl;
+		cout << "Enter Ctrl+Z for ending the stream..." << endl;
+	}*/
+
+	//FOR READING
+
+	//ifstream inf("test.txt");
+
+	//if (!inf)
+	//{
+	//	cerr << "FILE NOT FOUND..." << endl;
+	//	exit(0);
+	//}
+
+	//int id = 0;
+	//float cgpa = 0.0f;
+
+	//inf >> id >> cgpa;
+
+	//while (!inf.eof())
+	//{
+	//	cout << id << "   " << cgpa << endl;
+	//	inf >> id >> cgpa;
+	//}
+
+
+	//inf.close();
+}
